d21a1629631f067aaefcb0b061d28f149b909b9e
Zion Mezba <65642391+zionmezba@users.noreply.github.com>
2022-06-01 00:40:41 +0600

    Create 50A - Domino piling.cpp

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1e5
+#define ll long long
+
+void solve(){
+    int m,n;
+    cin >> m >> n;
+    cout << m*n/2;
+}
+
+
+int main()
+{
+    solve();
+    return 0;
+}
