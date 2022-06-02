c54a818fe8cded54425a4fe6cf1477da57edea46
Mezbaul Islam Zion <65642391+zionmezba@users.noreply.github.com>
2022-06-02 18:08:32 +0600

    Add files via upload

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1e5
+#define ll long long
+
+void solve()
+{
+    string str;
+    cin >> str;
+    str[0] = toupper(str[0]);
+    cout << str;
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
